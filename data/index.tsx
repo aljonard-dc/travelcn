e85f566779a43c75d4e3ff31f4c01699098e4b7a
Aljonard Dela Cruz <aljonarddc@gmail.com>
2025-01-04 21:22:08 +0800

    add navlinks in data

---
+++ data/index.tsx
@@ -0,0 +1,18 @@
+{/* NavLinks */}
+export const NavLinks = [
+    {
+        id:1,
+        title:"Home",
+        href:"#home"
+    },
+    {
+        id:2,
+        title:"Destinations",
+        href:"destinations/"
+    },
+    {
+        id:3,
+        title:"Festivals",
+        href:"festivals/"
+    },
+]
