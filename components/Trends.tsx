d0cf62882ebd06a5b835f8036db84722de543d7a
John Rey Luntayao <johnrey.luntayao23@gmail.com>
2025-01-05 16:20:57 +0800

    add card component

---
+++ components/Trends.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Card } from '@/lib/imports'
+
+const Trends = () => {
+  return (
+    <section className='flex justify-center items-center'>
+    </section>
+    
+  )
+}
+
+export default Trends
