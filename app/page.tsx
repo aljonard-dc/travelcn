01d437a219fc979ed94296878dd716c87d7523a1
John Rey Luntayao <johnrey.luntayao23@gmail.com>
2025-01-06 16:44:34 +0800

    add about component

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Hero from '@/components/Hero';
 import { 
     Header,
     Trends,
+    About,
   } from '@/lib/imports'
 
 export default function Home() {
@@ -11,6 +12,7 @@ export default function Home() {
       <div className='px-mobile-margin md:px-tablet-margin lg:px-laptop-margin xl:px-desktop-margin'>
         <Header />
         <Hero/>
+        <About />
         <Trends id={1}/>
         <Trends id={2}/>
       </div>
