d1b52ba852e98fc361551358aad7e7f261e1166b
johnrefani <jhnrefani@gmail.com>
2025-01-14 07:43:40 +0800

    Added animations to about, icon menu, qoutes, and added responsive year to footer

--- components/Qoutes.tsx
+++ components/Qoutes.tsx
@@ -1,21 +1,67 @@
-import React from 'react'
+"use client"
+
+import React, {useRef} from 'react'
 import { qoutesContent } from '@/data'
+import { motion, useInView } from 'framer-motion';
 const Qoutes = () => {
+
+  const sectionRef = useRef(null);
+  const isInView = useInView(sectionRef, { margin: "-50px"})
+
+
+  const containerVariants = {
+    hidden: {
+      opacity: 0, y: 10
+    },
+    visible: {
+      opacity: 1, y: 0, transition: {
+        duration: 0.8, ease: 'easeIn'
+      }
+    }
+
+    
+  }
+
+  const textVariants = {
+    hidden: {
+      opacity: 0, y: -10
+    },
+    visible: {
+      opacity: 1, y: 0, transition: {
+        duration: 0.8, ease: 'easeIn', delay: 0.2
+      }
+    }
+  }
+
+
+
   return (
-    <section className='bg-green-600 my-8 py-8 px-4 md:px-16 -mx-mobile-margin md:-mx-tablet-margin lg:-mx-laptop-margin xl:-mx-desktop-margin'>
+    <motion.section 
+    ref={sectionRef}
+    initial="hidden"
+    animate={isInView ? 'visible' : 'hidden'}
+    exit="hidden"
+    variants={containerVariants}
+    className='bg-green-600 my-8 py-8 px-4 md:px-16 -mx-mobile-margin md:-mx-tablet-margin lg:-mx-laptop-margin xl:-mx-desktop-margin'>
 
           {qoutesContent.map((content, idx) =>(
-            <div key={idx}>
+            <motion.div 
+            ref={sectionRef}
+            initial="hidden"
+            animate={isInView ? 'visible' : 'hidden'}
+            exit="hidden"
+            variants={textVariants}
+            key={idx}>
             <p className='text-center p-2  text-white-100 font-normal text-sm md:text-base'>
             {content.text}
             </p> 
-            </div>
+            </motion.div>
 
             ))
             
           }
         
-    </section>
+    </motion.section>
 
     
   )
